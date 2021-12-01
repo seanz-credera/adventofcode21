5cac6c6a7c49c82a8df72536f19481a5440d19f2
seanzhang-credera <90470725+seanz-credera@users.noreply.github.com>
2021-12-01 15:31:48 -0600

    Update advent1.R

--- advent1.R
+++ advent1.R
@@ -1,12 +1,6 @@
 library(tidyr)
-library(dplyr)
 library(zoo)
 
 sub <- read.table("input.txt")
-
 sub$V1 %>% diff %>% {. > 0} %>% sum()
-
-
 sub$V1 %>% rollapply(3, sum) %>% diff %>% {. > 0} %>% sum
-
-# 1538
