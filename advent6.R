b606c25def652f0cb12ab257e4769afac3099d49
seanzhang-credera <90470725+seanz-credera@users.noreply.github.com>
2021-12-06 11:31:09 -0600

    Add files via upload

--- advent6.R
+++ advent6.R
@@ -1,12 +1,10 @@
 library(dplyr)
 
-fish <- read.table("input6.txt")$V1 %>% 
-  strsplit(",") %>% 
-  unlist %>% 
-  sapply(as.integer)
+fish <- readLines("input6.txt") %>% 
+  strsplit(",") %>% unlist %>% sapply(as.integer)
 
 fish1 <- c("0" = 0, table(fish), "6" = 0, "7" = 0, "8" = 0)
-for (i in 1:80){
+for (i in 1:256){
   newfish <- fish1["0"]
   names(fish1) <- names(fish1)[c(9, 1:8)]
   fish1["6"] <- fish1["6"] + newfish
